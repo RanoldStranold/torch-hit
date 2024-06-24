93350aed45cb463ba321a01e85f43bf05c62767b
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-24 12:29:47 +0200

    Javadoc.

--- common/src/main/java/it/crystalnest/torch_hit/compat/SoulFired.java
+++ common/src/main/java/it/crystalnest/torch_hit/compat/SoulFired.java
@@ -7,9 +7,19 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.StandingAndWallBlockItem;
 
+/**
+ * Soul Fire'd compatibility utility.
+ */
 public final class SoulFired {
   private SoulFired() {}
 
+  /**
+   * Sets the targeted entity on fire depending on the kind of torch used to hit it.
+   *
+   * @param item item.
+   * @param entity target.
+   * @param seconds fire duration.
+   */
   public static void setOnFire(ItemStack item, Entity entity, int seconds) {
     if (item.getItem() instanceof StandingAndWallBlockItem torch && torch.getBlock() instanceof FireTyped fireTypedTorch) {
       FireManager.setOnFire(entity, seconds, fireTypedTorch.getFireType());
